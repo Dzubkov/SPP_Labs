0267fe901c8b7a25787c9876ef53ef75c2b65d25
Dzubkov <71589723+Dzubkov@users.noreply.github.com>
2021-12-14 22:24:21 +0300

    Add files via upload

---
+++ lab7/Lab7SPP/CParallel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Lab1SPP.Elements;
+namespace Lab7SPP
+{
+    public class CParallel
+    {
+        public void WaitAll(TaskDelegate[] delegates)
+        {
+            int threadLength = delegates.Length;
+            TaskQueue queue = new TaskQueue(threadLength);
+            using (var countDown = new CountdownEvent(threadLength))
+            {
+                    foreach (TaskDelegate task in delegates)
+                    {
+                        queue.EnqueueTask(() =>
+                        {
+                            task();
+                            countDown.Signal();
+                        });
+                    }
+                countDown.Wait();
+            }
+            Console.WriteLine("Job well done!");
+            queue.Dispose();
+        }
+        public void StartAll(TaskDelegate[] delegates)
+        {
+            int threadLength = delegates.Length;
+            TaskQueue queue = new TaskQueue(threadLength);
+            foreach (TaskDelegate task in delegates)
+            {
+                queue.EnqueueTask(() =>
+                {
+                    task();
+
+                });
+            }
+            queue.Dispose();
+        }
+    }
+}
