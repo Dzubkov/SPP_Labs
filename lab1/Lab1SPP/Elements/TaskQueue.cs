dcf06f5260c448d6926cc78ad70da70c3c6be22e
Dzubkov <71589723+Dzubkov@users.noreply.github.com>
2021-12-14 22:25:36 +0300

    Add files via upload

---
+++ lab1/Lab1SPP/Elements/TaskQueue.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab1SPP.Elements
+{
+    public class TaskQueue : IDisposable
+    {
+        private LinkedList<Thread> pool;
+        private bool disposed;
+        private int capacity;
+        private bool disposingProc;
+        private LinkedList<TaskDelegate> taskQueue;
+        public TaskQueue(int capacity)
+        {
+            pool = new LinkedList<Thread>();
+            taskQueue = new LinkedList<TaskDelegate>();
+            this.capacity = capacity;
+            for(int i = 0; i<capacity; i++)
+            {
+                var thread = new Thread(WorkingProcess) { Name = string.Concat("Thread {1}", i) };
+                thread.Start();
+                pool.AddLast(thread);
+            }
+        }
+        public int Size()
+        {
+            return capacity;
+        }
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (taskQueue)
+            {
+                if (disposingProc)
+                {
+                    throw new InvalidOperationException("This Pool is in process of disposing.");
+                }
+                if (disposed)
+                {
+                    throw new ObjectDisposedException("This Pool has been already disposed");
+                }
+                taskQueue.AddLast(task);
+                Monitor.PulseAll(taskQueue);
+            }
+        }
+        public void Dispose()
+        {
+            var waitForThreads = false;
+            lock (taskQueue)
+            {
+                if (!disposed)
+                {
+                    GC.SuppressFinalize(this);
+
+                    disposingProc = true;
+                    while (this.taskQueue.Count > 0)
+                    {
+                        Monitor.Wait(taskQueue);
+                    }
+
+                    disposed = true;
+                    Monitor.PulseAll(taskQueue);
+                    waitForThreads = true;
+                }
+            }
+            if (waitForThreads)
+            {
+                foreach (var thread in pool)
+                {
+                    thread.Join();
+                }
+            }
+        }
+        private void WorkingProcess()
+        {
+            TaskDelegate task = null;
+            while (true)
+            {
+                lock (taskQueue)
+                {
+                    while (true)
+                    {
+                        if (disposed)
+                        {
+                            return;
+                        }
+                        if (pool.First.Value !=null && ReferenceEquals(Thread.CurrentThread, pool.First.Value) && taskQueue.Count> 0)
+                        {
+                            task = taskQueue.First.Value;
+                            taskQueue.RemoveFirst();
+                            pool.RemoveFirst();
+                            Monitor.PulseAll(taskQueue);
+                            break;
+                        }
+                        Monitor.Wait(taskQueue);
+                    }
+                }
+                task();
+                lock (taskQueue)
+                {
+                    pool.AddLast(Thread.CurrentThread);
+                }
+                task = null;
+            }
+        }
+    }
+}
---
+++ lab1/Lab1SPP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Lab1SPP.Elements;
+
+namespace Lab1SPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskDelegate del = delegate ()
+            {
+                Console.WriteLine("Some");
+            };
+            TaskDelegate del2 = delegate ()
+            {
+                Console.WriteLine("SomeThing");
+            }; TaskDelegate del3 = delegate ()
+            {
+                Console.WriteLine("SOmething");
+            };
+            TaskDelegate del4 = delegate ()
+            {
+                Console.WriteLine("something");
+            };
+            TaskQueue queue = new TaskQueue(8);
+            queue.EnqueueTask(del);
+            queue.EnqueueTask(del2);
+            queue.EnqueueTask(del3);
+            queue.EnqueueTask(del4);
+            queue.Size();
+            queue.Dispose();
+        }
+
+    }
+}
